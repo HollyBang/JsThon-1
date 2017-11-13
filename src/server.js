6421a94b08223f54c7701f50ee73f0fadd870ad3
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-13 15:56:56 +0300

    Поменяли node-telegram-bot-api  на telegraf.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,13 @@
-const Promise = require('bluebird');
-Promise.config({
-  cancellation: false
-});
+const Telegraf = require('telegraf')
 
-const TelegramBot = require('node-telegram-bot-api');
+const bot = new Telegraf('496344958:AAFsQLwrqNcw8iBNrtTfmXNrgYFn2KSJmh4')
+bot.start((ctx) => {
+  console.log('started:', ctx.from.id)
+  return ctx.reply('Welcome!')
+})
+bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '496344958:AAFsQLwrqNcw8iBNrtTfmXNrgYFn2KSJmh4';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-
-bot.on('message', (msg) => {
-    
-let Hi = "hi";
-if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-bot.sendMessage(msg.chat.id,"Sam, che kak");
-} 
-    
-});
+bot.startPolling()
