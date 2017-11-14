46683b94163626a5941fd17e57ec72809e1c64f6
Andrey Barchuk <ndreybar@gmail.com>
2017-11-14 11:06:42 +0200

    added interface spam

--- src/server.js
+++ src/server.js
@@ -1,4 +1,19 @@
 const Telegraf = require('telegraf')
+const { reply } = Telegraf
+
+
+//spam store
+var spamModule = (function(){
+  let spamStore = [];
+  return {
+      getSpamStore: function(){
+           return spamStore;   
+      },
+      pushSpamWord: function(spam){
+           spamStore.push(spam) 
+      }
+  };
+})();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => {
@@ -6,6 +21,18 @@ bot.start((ctx) => {
   return ctx.reply('Welcome!')
 })
 bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
+//get all spam in store
+bot.command('getspam', (ctx) => { 
+   let allSpam = spamModule.getSpamStore().join(', ')
+ return ctx.reply(`Список слов в спаме: ${allSpam}`)
+})
+//add spam word
+bot.hears(/(\/spam) \w+/, (ctx) => {
+  let receivedMessage = ctx.message.text;
+  let spamWord = receivedMessage.split(' ')[1];
+  spamModule.pushSpamWord(arrMes);
+  return ctx.reply(`Добавил в спам ${spamWord}`)
+})
 bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
 bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
